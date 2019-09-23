78dd82c44ad8c649deb78638bb53b17fb39e4864
Adailson Aguiar <adailsonacj@live.com>
2019-09-23 19:16:01 -0300

    feat(card): recuperar posicao do card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,41 @@
-import React from 'react'
-import { Container, Label } from './styles'
-import { useDrag } from 'react-dnd'
+import React, { useRef } from 'react';
+import { Container, Label } from './styles';
+import { useDrag, useDrop } from 'react-dnd';
 
-export default function Card({ data }) {
+export default function Card({ data, index }) {
+    const ref = useRef();
 
     const [{ isDragging }, dragRef] = useDrag({
-        item: { type: 'CARD' },
+        item: { type: 'CARD', index },
         collect: monitor => ({
             isDragging: monitor.isDragging(),
-        })
-    })
+        }),
+    });
+
+    const [, dropRef] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor) {
+            const draggedIndex = item.index;
+            const targetIndex = index;
+
+            if (draggedIndex == targetIndex) {
+                return;
+            }
+            console.log(item);
+        },
+    });
+
+    dragRef(dropRef(ref));
+
     return (
-        <Container ref={dragRef} isDragging={isDragging}>
+        <Container ref={ref} isDragging={isDragging}>
             <header>
-                {data.labels.map(label => <Label color={label} />)}
+                {data.labels.map(label => (
+                    <Label key={label} color={label} />
+                ))}
             </header>
             <p>{data.content}</p>
             {data.user && <img src={data.user} alt="" />}
         </Container>
-    )
+    );
 }
